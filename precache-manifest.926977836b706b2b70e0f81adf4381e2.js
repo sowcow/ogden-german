bde3a04b02c51a3ca99e86e38399db0c9754b087
Alexander K <code.alexander.k@gmail.com>
2019-10-01 16:02:15 +0500

    Updates

---
+++ precache-manifest.926977836b706b2b70e0f81adf4381e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670afa88f7fddfeebcba6f09c41619e6",
+    "url": "/ogden-german/index.html"
+  },
+  {
+    "revision": "eb9e8eb13b0f3f6b6f75",
+    "url": "/ogden-german/static/js/2.8e0d17ac.chunk.js"
+  },
+  {
+    "revision": "6b369e6e62b53d052eba",
+    "url": "/ogden-german/static/js/main.6b7a30e4.chunk.js"
+  },
+  {
+    "revision": "90d5275201e6b7b33c88",
+    "url": "/ogden-german/static/js/runtime~main.261cd323.js"
+  }
+]);
